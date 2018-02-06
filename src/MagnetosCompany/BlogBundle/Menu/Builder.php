9a0e8df09fb57fb2fa54ef2500f1005162814917
Aleks Sushitskiy <magnetos.ua@gmail.com>
2018-02-06 14:29:56 +0200

    Updated knp-menu to good view

--- src/MagnetosCompany/BlogBundle/Menu/Builder.php
+++ src/MagnetosCompany/BlogBundle/Menu/Builder.php
@@ -14,11 +14,28 @@ class Builder implements ContainerAwareInterface
     {
     $menu = $factory->createItem('root');
 
-    $menu->addChild('Главная', ['route' => 'blog_homepage']);
-    $menu->addChild('Добавить статью', ['route' => 'blog_add_post']);
-    $menu->addChild('Контакты', ['route' => 'blog_admin']);
-    $menu->addChild('Регистрация', ['route' => 'blog_register']);
-    $menu->addChild('Войти', ['route' => 'blog_login']);
+    $menu
+        ->addChild('Главная', ['route' => 'blog_homepage'])
+        ->setAttribute('class', 'nav-item active')
+        ->setLinkAttribute('class', 'nav-link');
+
+    $menu->setChildrenAttribute('class', 'navbar-nav ml-auto');
+    $menu
+        ->addChild('Добавить статью', ['route' => 'blog_add_post'])
+        ->setAttribute('class', 'nav-item')
+        ->setLinkAttribute('class', 'nav-link');
+    $menu
+        ->addChild('Контакты', ['route' => 'blog_admin'])
+        ->setAttribute('class', 'nav-item')
+        ->setLinkAttribute('class', 'nav-link');
+    $menu
+        ->addChild('Регистрация', ['route' => 'blog_register'])
+        ->setAttribute('class', 'nav-item')
+        ->setLinkAttribute('class', 'nav-link');
+    $menu
+        ->addChild('Войти', ['route' => 'blog_login'])
+        ->setAttribute('class', 'nav-item')
+        ->setLinkAttribute('class', 'nav-link');
 
     return $menu;
     }
