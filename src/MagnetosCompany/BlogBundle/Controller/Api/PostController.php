74cc2c0d515dbf0d4c07a627a6cd88f7bf93e0ad
Aleks Sushitskiy <magnetos.ua@gmail.com>
2018-03-10 23:46:57 +0200

    Created listAction that can return all Posts

--- src/MagnetosCompany/BlogBundle/Controller/Api/PostController.php
+++ src/MagnetosCompany/BlogBundle/Controller/Api/PostController.php
@@ -31,26 +31,66 @@ class PostController extends Controller
 
         $em = $this->getDoctrine()->getManager();
         $em->persist($post);
-        $em->flush();
+        //$em->flush();
 
-        $response = new Response('It worked. Believe me - I\'m an API', 201);
-        $response->headers->set('Location', '/some/programmer/url');
+        $data = $this->serializePost($post);
+        $response = new JsonResponse($data, 201);
+
+        $postUrl = $this->generateUrl(
+            'api_posts_show',
+            ['title' => $post->getTitle()]
+        );
+        $response->headers->set('Location', $postUrl);
+
+
         return $response;
 
     }
 
     /**
-     * @Route("/api/posts/{title}")
+     * @Route("/api/posts/{title}", name="api_posts_show")
      * @Method("GET")
      */
     public function showAction($title)
     {
         $post = $this->getDoctrine()->getRepository(Post::class)->findOneBy(['title' => $title]);
-        $data = [
+        if (!$post) {
+            throw $this->createNotFoundException(sprintf(
+                'No post found with title "%s"',
+                $title
+            ));
+        }
+
+        $data = $this->serializePost($post);
+        $response = new Response(json_encode($data), 200);
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+
+    /**
+     * @Route("/api/posts")
+     * @Method("GET")
+     */
+    public function listAction()
+    {
+        $posts = $this->getDoctrine()
+            ->getRepository(Post::class)
+            ->findAll();
+        $data = ['posts' => []];
+        foreach ($posts as $post) {
+            $data['postss'][] = $this->serializePost($post);
+        }
+        $response = new Response(json_encode($data), 200);
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+
+    private function serializePost(Post $post)
+    {
+        return [
             'title' => $post->getTitle(),
             'article' => $post->getArticle(),
             'linkToImage' => $post->getLinkToImage(),
         ];
-        return new Response(json_encode($data), 200);
     }
 }
